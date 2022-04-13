de589ec32cd4d4d3296ae3ecf186572c6c521ede
Noah Pikielny <np299@cornell.edu>
2022-04-12 23:34:14 -0400

    Initial Commit

---
+++ CVPlayground/CVPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVPlaygroundApp.swift
+//  CVPlayground
+//
+//  Created by Noah Pikielny on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CVPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
